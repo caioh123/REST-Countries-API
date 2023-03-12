37349ecd114de3cdcc5a94d8f6869749984fddff
caioh123 <caionwebdev@gmail.com>
2023-03-12 11:41:50 -0300

    feat: added header component

---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  width: 100%;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content:space-between
+
+  p {
+    font-size: 2rem;
+    color: black;
+    
+
+  }
+`
--- src/pages/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.css'
-
-export function Dashboard() {
-  return (
-    <h1>
-      oi
-    </h1>
-  )
-}
