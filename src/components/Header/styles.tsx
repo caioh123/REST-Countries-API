326b58df65e3b0f0a4963b4fab065dd12f9c66e9
caioh123 <caionwebdev@gmail.com>
2023-03-12 13:05:38 -0300

    feat: added responsive design for header component

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,38 +1,36 @@
 import styled from 'styled-components';
+import { theme } from '../../static/theme';
 
 export const Container = styled.div`
   width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content:space-between;
+  display: 
+5
+  justify-content: space-between;
+  background-color: ${theme.pallete.background.all};
 
   p {
     font-size: 1.4rem;
     padding: 1rem;
     font-weight: bold;
-
-    
-
+    background-color: ${theme.pallete.background.all};
   }
 
   div {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    background-color: ${theme.pallete.background.all};
 
-    @media only screen and (min-width: 768px){
+    @media only screen and (min-width: 768px) {
       width: 40%;
-      
     }
 
-    @media only screen and (min-width: 992px){
+    @media only screen and (min-width: 992px) {
       width: 30%;
-      
     }
 
-    @media only screen and (min-width: 1200px){
+    @media only screen and (min-width: 1200px) {
       width: 10%;
-      
     }
 
     p {
@@ -40,4 +38,4 @@ export const Container = styled.div`
       font-weight: normal;
     }
   }
-`
+`;
